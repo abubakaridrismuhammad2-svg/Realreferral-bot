713975f9ab1d00d29d14316300692e05bbbb2c46
abubakaridrismuhammad2-svg <abubakaridrismuhammad2@gmail.com>
2026-09-24 16:42:06 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
-export default async function handler(req, res) {
-  const BOT_TOKEN = process.env.BOT_TOKEN;
-  res.status(200).send("Bot is Running! RealReferral Bot Active");
-}         
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.BOT_TOKEN);
+
+module.exports = async (req, res) => {
+  if (req.method === 'GET') {
+    return res.status(200).send('Bot is Running! RealReferral Bot Active');
+  }
+  if (req.method === 'POST') {
+    const msg = req.body.message;
+    if (msg) {
+      const chatId = msg.chat.id;
+      if (msg.text === '/start') {
+        await bot.sendMessage(chatId, `Assalamu Alaikum! Barka da zuwa RealReferral Bot. ID dinka: ${chatId}`);
+      }
+    }
+    return res.status(200).send('ok');
+  }
+  return res.status(405).send('Method not allowed');
+};
